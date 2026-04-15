ca7ce55269cdf1623cf54668b345fb3e1f4fab6b
JoseMY19 <joseedsonmilla@gmail.com>
2026-04-15 00:51:50 -0500

    feat: proyecto completo Spring Boot Kitchen Hack

---
+++ src/test/java/com/kitchenhack/apikitchen/ApikitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenhack.apikitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApikitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
