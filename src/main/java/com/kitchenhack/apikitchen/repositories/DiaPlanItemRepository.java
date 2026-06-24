827ba8a24b374b35ac329e5aac4edb6a2830c8ee
Jose Milla <151081849+JoseMY19@users.noreply.github.com>
2026-06-24 15:38:09 -0500

    refactor: remove extra entities and services to reduce schema to 9 tables

--- src/main/java/com/kitchenhack/apikitchen/repositories/DiaPlanItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kitchenhack.apikitchen.repositories;
-
-import com.kitchenhack.apikitchen.entities.DiaPlanItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiaPlanItemRepository extends JpaRepository<DiaPlanItem, Integer> {
-}
